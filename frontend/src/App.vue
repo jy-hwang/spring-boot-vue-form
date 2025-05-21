0ccef8f6299291c9d54f3af4370ae8b954a5405b
jyhwang <stratojy@gmail.com>
2025-05-21 20:28:20 +0900

    feat. Frontend - 레이아웃 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,40 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <header>신청서 애플리케이션</header>
+  <header>
+    <div class="container">
+      <h1>신청서 애플리케이션</h1>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </main>
+  <footer>
+    <div class="container">&copy; 2025. Example. All rights reserved.</div>
+  </footer>
 </template>
 
-<style>
+<style lang="scss">
+header {
+  padding: 30px;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 30px;
 
+  h1 {
+    font-size: 20px;
+    font-weight: bold;
+  }
+}
+
+main {
+  padding: 30px 0;
+}
+
+footer {
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #ddd;
+}
 </style>
-
